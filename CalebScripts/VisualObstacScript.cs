010b7170c0232e7a75d1eb2ee2a00ba164d5147c
Caleb Strait <caleb.strait@gmail.com>
2016-06-04 14:11:19 -0400

    Add files via upload

---
+++ CalebScripts/VisualObstacScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualObstacScript : MonoBehaviour {
+
+	public GameObject trackedObstac;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - trackedObstac.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = trackedObstac.transform.position + offset;
+	}
+}
