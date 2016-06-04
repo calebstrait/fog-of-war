c6cd338b5db0054df0c3d66b94487e0ed49f5459
Caleb Strait <caleb.strait@gmail.com>
2016-06-04 14:04:50 -0400

    Add files via upload

---
+++ Assets/InstructionText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InstructionText : MonoBehaviour {
+
+	public CellGrid CellGrid;
+	public Text instruction;
+	void Start () {
+		instruction = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CellGrid.result == -1) {
+			instruction.text = "ATTACKERS WIN!";
+			instruction.color = Color.red;
+		} else if (CellGrid.result == 1) {
+			instruction.text = "DEFENDERS WIN!";
+			instruction.color = Color.blue;
+		} else if (CellGrid.CurrentPlayerNumber == 0) {
+			instruction.text = "INFILTRATE AND LOOT!";
+			instruction.color = Color.green;
+		} else {
+			instruction.text = "POSITION YOUR TROOPS!";
+			instruction.color = Color.green;
+		}
+	}
+}
